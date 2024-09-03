ace971af79356a06bf5cbf10daa21036ae3bcb14
hasantalib <hasan_talib@icloud.com>
2024-09-03 17:36:12 -0400

    Update config.js
    original setup was initializing Firebase correctly. I kept the initialization of app, auth, and firestore in the same order, which is perfectly fine.
    
    I added the initialization of GoogleAuthProvider (googleProvider) to make sure it’s ready for use if weneed it for authentication with Google.
    
    also, the exports are now clearly defined, making it clear what parts of Firebase are available for use throughout the application.

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,13 +1,9 @@
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAJUjMQYiwGtp0d6q6I80VJ6wxN_0BOki8",
   authDomain: "careercanvas-f826a.firebaseapp.com",
@@ -18,9 +14,13 @@ const firebaseConfig = {
   measurementId: "G-693QS0Y22N"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
 const auth = getAuth(app);
 const firestore = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
 
-// Export auth and provider
-export { auth, GoogleAuthProvider, firestore };
+// Export Firebase services
+export { auth, firestore, googleProvider };
