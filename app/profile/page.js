f3eeba19edca9e16e1063b06ae018cd51003713d
Osman Mehmood <osman.mehmood2@gmail.com>
2024-09-04 19:20:36 +0500

    Update page.js

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,3 +1,4 @@
+'use client'
 import { useState } from 'react';
 import { firestore } from '../../firebase/config';  // Ensure your Firebase config is correctly set up
 import { Box, TextField, Button, Typography } from '@mui/material';
