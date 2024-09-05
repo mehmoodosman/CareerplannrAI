18f98aca0821f1d9ba89d72e35716af7afcf6fff
Osman Mehmood <osman.mehmood2@gmail.com>
2024-09-06 01:58:13 +0500

    Update route.js

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -21,11 +21,7 @@ You should return in the following JSON format:
 `;
 
 export async function POST(req){
-    const openai = new OpenAI({
-      apiKey: process.env.OPENROUTER_API_KEY,
-      baseURL: "https://openrouter.ai/api/v1",
-    });
-
+    const openai = new OpenAI(process.env.OPENAI_API_KEY)
     const data = await req.text()
 
     const completion = await openai.chat.completions.create({
@@ -35,9 +31,9 @@ export async function POST(req){
         ],
         model: "openai/gpt-3.5-turbo",
         response_format: {type: 'json_object'},
-    });
+    })
     
-    const flashcards = JSON.parse(completion.choices[0].message.content);
+    const flashcards = JSON.parse(completion.choices[0].message.content)
 
-    return NextResponse.json(flashcards.flashcards);
+    return NextResponse.json(flashcards.flashcards)
 }
