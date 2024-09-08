aa03d876577107294dc29bfe1e7fe27849c87ee6
Irvin Tancioco <irvin.tanc@gmail.com>
2024-09-07 21:41:24 -0700

    update mentors

--- app/mentors/page.js
+++ app/mentors/page.js
@@ -1,30 +1,33 @@
+'use client';
 import {
     Box,
     Grid,
     Typography,
   } from '@mui/material'
 
-
+import { useState, useEffect, useRef } from "react";
 export default function Mentors() {
+    const [mentors, setMentors] = useState({})
 
 
     return ( <>
-          {recommendations && recommendations.length > 0 && (
+        Mentors
+          {mentors && mentors.length > 0 && (
             <Grid item>
-              <Typography variant='h4' color={coral} gutterBottom>Top {recommendations?.length} Candidates</Typography>
+              <Typography variant='h4' color={coral} gutterBottom>Top {mentors?.length} Mentors</Typography>
               <Box p={2}>
-                {recommendations?.map((candidate, index) => (
+                {mentors?.map((mentor, index) => (
                   <Box key={index} p={2} m={1} sx={{borderRadius: 4, border: '1px solid #ccc', backgroundColor: 'white'}}>
-                    <Typography variant='body1' color={green} fontWeight={700}>{candidate.name}</Typography>
-                    <Typography variant='body2'>{candidate.location}</Typography>
+                    <Typography variant='body1' color={green} fontWeight={700}>{mentor.name}</Typography>
+                    <Typography variant='body2'>{mentor.location}</Typography>
                     <Typography variant='body2'>
-                    { candidate.tech_stacks
+                    { mentor.tech_stacks
                         .map((stack) => stack.split(':')[0].trim())  // Extract the tech names
                         .join(', ')  // Join the stack names into a single string
                     }
                     </Typography>
 
-                    <Typography variant='body2'>{candidate.comment}</Typography>
+                    <Typography variant='body2'>{mentor.comment}</Typography>
                   </Box>
                 ))}
               </Box>
